9aa4e04f7cabea9738b391cc8e020ba7c5865a45
Ronaldo Balsimelli <ronaldobalsimelli@hotmail.com>
2025-04-19 21:04:21 -0300

    Atualizacoes de CSS e Adicao pagina quando clica na serie

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,9 +1,17 @@
+require 'net/http'
+require 'json'
+
 class ShowsController < ApplicationController
   def index
-    @shows = Show.all
+    url = URI("https://api.tvmaze.com/shows")
+    response = Net::HTTP.get(url)
+    @shows = JSON.parse(response)
   end
 
   def show
-    @show = Show.find(params[:id])
+    show_id = params[:id]
+    url = URI("https://api.tvmaze.com/shows/#{show_id}?embed[]=episodes&embed[]=cast")
+    response = Net::HTTP.get(url)
+    @show = JSON.parse(response)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root "shows#index"
-
-  resources :shows, only: [:index, :show] do
-    resources :episodes, only: [:index, :show]
-    resources :actors, only: [:index, :show]
-  end
+  root 'shows#index'
+  resources :shows, only: [:index, :show]
 end
